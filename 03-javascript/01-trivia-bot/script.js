b3a5b1ca3be0955bd1316806c0825bbc93ae9222
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-08-01 23:24:32 +0300

    feat: add Trivia Bot lab solution

---
+++ 03-javascript/01-trivia-bot/script.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia Bot";
+let botLocation = "your computer";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+
+codingFact = "Did you know that " + favoriteLanguage + " can be used for both front-end and back-end development?";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was originally created in just 10 days!";
+console.log(codingFact);
+
+codingFact = "Many websites rely on " + favoriteLanguage + " to make them interactive.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
