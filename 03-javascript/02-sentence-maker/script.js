4dda1404f39c8be1414bd401665431757b92d254
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-08-02 23:57:13 +0300

    feat(javascript.sentence-maker): add lab solution

---
+++ 03-javascript/02-sentence-maker/script.js
@@ -0,0 +1,24 @@
+let adjective = "curious";
+let noun = "cat";
+let noun2 = "fish";
+let place = "garden";
+let adjective2 = "small";
+let verb = "happy";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + 
+". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+console.log("First story: " + firstStory);
+
+adjective = "brave";
+noun = "knight";
+noun2 = "apples";
+place = "castle";
+adjective2 = "shiny";
+verb = "proud";
+
+let secondStory  = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + 
+". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
